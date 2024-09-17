1780ad74a35d93ee7af5fcd16e38bbc0aceb8493
L <luparedes100@gmail.com>
2024-09-17 16:41:49 -0400

    added sidebar icon to add more chats

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -1,6 +1,7 @@
 import { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
 import { getServerAuthSession } from "note/server/auth";
+import { BsPlus } from "react-icons/bs";
 
 export const SidebarWidth = "w-16";
 
@@ -11,14 +12,25 @@ export default async function Sidebar() {
 
     return(<div>
         <div className="relative top-0 left-0 bg-black h-screen w-16 m-0 flex flex-col">
-            <SidebarIcon icon={<img src={userProfileImage}/>} href="/profile" />
+            <SidebarImageIcon icon={<img src={userProfileImage}/>} href="/profile" />
+            <SidebarIcon icon={<BsPlus size="32" />} href="" />
         </div>
     </div>)
 }
 
-const SidebarIcon = ({ icon, href}: { icon: JSX.Element, href: Url }) => {
+const SidebarImageIcon = ({ icon, href}: { icon: JSX.Element, href: Url }) => {
     return(
         <div className="sidebar-icon">
+            <Link href={href}>
+                {icon}
+            </Link>
+        </div>
+    );
+}
+
+const SidebarIcon = ({ icon, href}: { icon: JSX.Element, href: Url }) => {
+    return(
+        <div className="sidebar-icon bg-slate-500 hover:bg-blue-600 text-white hover:text-slate-200 rounded-3xl hover:rounded-2xl transition-all ease-linear duration-150">
             <Link href={href}>
                 {icon}
             </Link>
