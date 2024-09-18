a417259c0bfbb8761510360cb428375d9b9fdfbf
do262431@gmail.com <do262431@gmail.com>
2024-09-18 14:42:28 -0400

    Fixed Logic error

--- src/app/_components/sidebar-chat-icons.tsx
+++ src/app/_components/sidebar-chat-icons.tsx
@@ -1,4 +1,4 @@
-import { getServerAuthSession } from "note/server/auth"
+import { getServerAuthSession } from "note/server/auth";
 import { api } from "note/trpc/server";
 import { SidebarIcon } from "./sidebar-icons";
 import { BsFillHexagonFill } from "react-icons/bs";
@@ -8,28 +8,19 @@ export default async function SidebarChatIcons() {
     const id = session?.user.id;
 
     // should never happen
-    if(!id) return;
+    if (!id) return;
 
     const user = await api.user.getById(id);
 
-    const chats = user?.usersToChats.map(
-        ({ chat }) => {
-            return {
-                ...chat,
-                key: chat.id,
-            }
-        });
-    
-    return(
+    const chats = user?.usersToChats.map(({ chat }) => chat);
+
+    return (
         <ul>
-            {chats?.map((chat, i) => {
-                return(
-                    <li key={chat.key}>
-                        <SidebarIcon icon={<BsFillHexagonFill />} href={`/chat/${chat.id}`} />
-                    </li>
-                    
-                )
-            })}
+            {chats?.map((chat) => (
+                <li key={chat.id}>
+                    <SidebarIcon icon={<BsFillHexagonFill />} href={`/chat/${chat.id}`} />
+                </li>
+            ))}
         </ul>
-    )
+    );
 }
