b6991e8621076ccd1780c911295f8582f6a21b40
L <luparedes100@gmail.com>
2024-09-18 14:44:35 -0400

    second try to fix key error

--- src/app/_components/sidebar-chat-icons.tsx
+++ src/app/_components/sidebar-chat-icons.tsx
@@ -2,6 +2,7 @@ import { getServerAuthSession } from "note/server/auth"
 import { api } from "note/trpc/server";
 import { SidebarIcon } from "./sidebar-icons";
 import { BsFillHexagonFill } from "react-icons/bs";
+import React from "react";
 
 export default async function SidebarChatIcons() {
     const session = await getServerAuthSession();
@@ -21,7 +22,9 @@ export default async function SidebarChatIcons() {
         <div>
             {chats?.map((chat, i) => {
                 return(
+                    <React.Fragment key={i} >
                     <SidebarIcon icon={<BsFillHexagonFill key={i} />} href={`/chat/${chat.id}`} key={i} />
+                    </React.Fragment>
                 )
             })}
         </div>
