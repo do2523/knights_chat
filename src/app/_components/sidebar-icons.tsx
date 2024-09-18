7645d73ddd5a7a2add4e38067ba07cf02e72387b
L <luparedes100@gmail.com>
2024-09-18 14:52:10 -0400

    changed any type for onClick

--- src/app/_components/sidebar-icons.tsx
+++ src/app/_components/sidebar-icons.tsx
@@ -1,5 +1,6 @@
 import Link from "next/dist/client/link";
 import { Url } from "next/dist/shared/lib/router/router";
+import { MouseEventHandler } from "react";
 
 export function SidebarImageIcon({ icon, href}: { icon: JSX.Element, href: Url }) {
     return(
@@ -11,7 +12,7 @@ export function SidebarImageIcon({ icon, href}: { icon: JSX.Element, href: Url }
     );
 }
 
-export function SidebarIcon({ icon, href, onClick }: { icon: JSX.Element, href?: Url, onClick?: any }) {
+export function SidebarIcon({ icon, href, onClick }: { icon: JSX.Element, href?: Url, onClick?: MouseEventHandler<HTMLElement> }) {
     if(!href) {
         return(
             <div onClick={onClick} className="sidebar-icon bg-slate-500 hover:bg-blue-600 text-white hover:text-slate-200 rounded-3xl hover:rounded-2xl transition-all ease-linear duration-150">
